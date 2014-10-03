a473b70f72ecd0e2804c7ff19949e6b94844e561
Roy van de Water <dev+roy@integrumtech.com>
2014-10-03 09:59:04 -0700

    Base4 encode and decode

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var b4decode, b4encode;
+
+b4decode = function(base4data){
+  var hexdata = parseInt(base4data, 4).toString(16);
+
+  var data = '';
+  for(var i=0; i < hexdata.length; i += 2) {
+    var fragment = hexdata[i] + hexdata[i+1];
+    data += String.fromCharCode(parseInt(fragment, 16));
+  }
+  return data;
+};
+
+b4encode = function(data){
+  var hexdata = new Buffer(data).toString('hex');
+  console.log(hexdata);
+  return parseInt(hexdata, 16).toString(4);
+};
+
+module.exports = {
+  b4decode: b4decode,
+  b4encode: b4encode
+};
